bf5999359feb2b177e7b3f62ad0120ca52088798
im <maryna.todorishyna@ekreative.com>
2018-01-21 23:59:30 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
 
 class App extends Component {
     render() {
         return (
             <div>
-                MaDe
+                <Router>
+                    <header>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/backlog">Backlog</Link>
+                                </li>
+                                <li>
+                                    <Link to="/active">Active</Link>
+                                </li>
+                                <li>
+                                    <Link to="/archive">Archive</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+                </Router>
             </div>
         );
     }
